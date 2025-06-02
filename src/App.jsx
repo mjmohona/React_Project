c44c4307dd198bcb51eed5613dedff8f742be0ae
mjmohona <mj.mohonajahan@gmail.com>
2025-06-02 17:16:32 +0600

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,8 @@
+import React from "react";
+import "./App.css";
 
 function App() {
-  return (
-    <div className="app">
-      Welcome to mohona's world
-    </div>
-  );
+  return <div className="app">Welcome to mohona&apos;s world</div>;
 }
 
 export default App;
